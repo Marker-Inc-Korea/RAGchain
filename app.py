36cb200c0d9c6b517ecf7bbceec44882419626ab
jeffrey <vkefhdl1@gmail.com>
2023-07-26 01:41:06 +0900

    Refactor app.py to improve variable naming and adjust module imports
    A series of changes have been made to the app.py file to increase code clarity and maintainability. First, several lower-case constant values, such as `device` and `model_type`, have been capitalized to improve readability and follow accepted Python conventions for constant naming. The imports have been adjusted to remove `ingest_texts` which is no longer used, and replace it with the `LangchainRetriever` class from the `retrieve` module. The `make_qa` function was also replaced with `make_llm_chain` function for better QR performance. The `save` method is now used to store the split document texts. Also, a comment is added to remind adding file cache for the gradio version.

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 import gradio as gr
 
-from ingest import load_single_document, split_documents, ingest_texts
+from ingest import load_single_document, split_documents
 from model import load_model
 from dotenv import load_dotenv
 
-from run_localGPT import make_qa, get_answer
+from retrieve import LangchainRetriever
+from run_localGPT import make_llm_chain, get_answer
 from utils import slice_stop_words
 from vectorDB import DB
 from embed import Embedding
@@ -15,26 +16,28 @@ load_dotenv()
 
 STOP_WORDS = ["#", "답변:", "응답:", "맥락:", "?"]
 
-device = "cuda"
-model_type = "OpenAI"
-llm = load_model(model_type)
-embedding_type = "OpenAI"
-db = DB('pinecone', Embedding(embed_type=embedding_type).embedding()).load()
-retriever = db.as_retriever()
+DEVICE = "cuda"
+MODEL_TYPE = "OpenAI"
+llm = load_model(MODEL_TYPE)
+EMBEDDING_TYPE = "OpenAI"
+DB_TYPE = "chroma"
+embeddings = Embedding(embed_type=EMBEDDING_TYPE, device_type=DEVICE).embedding()
+retriever = LangchainRetriever.load(db_type=DB_TYPE, embedding=embeddings)
 
 
 def ingest(files) -> str:
+    # TODO : add file cache for gradio version
     file_paths = [f.name for f in files]
     documents = [load_single_document(path) for path in file_paths]
     texts = split_documents(documents)
-    ingest_texts('cuda', 'pinecone', 'OpenAI', texts)
+    retriever.save(texts)
     return "Ingest Done"
 
 
 def make_answer(text):
-    qa = make_qa(llm, retriever)
+    chain = make_llm_chain(llm)
     # Get the answer from the chain
-    q, answer, docs = get_answer(qa, text)
+    answer, docs = get_answer(chain, retriever, text)
     answer = slice_stop_words(answer, STOP_WORDS)
     document_source = ",\n".join([doc.metadata["source"].split("/")[-1] for doc in docs])
     return answer, document_source
@@ -44,10 +47,10 @@ with gr.Blocks(analytics_enabled=False) as demo:
     gr.HTML(
         f"""<div style="text-align: center; max-width: 500px; margin: 0 auto;">
                 <div>
-                <h1>KoPrivateGPT with {model_type}</h1>
+                <h1>KoPrivateGPT with {MODEL_TYPE}</h1>
                 </div>
                 <div>
-                Demo runs on {device}
+                Demo runs on {DEVICE}
                 </div>
             </div>"""
     )
