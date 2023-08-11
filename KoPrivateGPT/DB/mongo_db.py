7773b7d7ba0d3b54627f0510d94323c49ccf7dd4
kimbwook <bwook00@naver.com>
2023-08-11 23:13:35 +0900

    add mongo_db.py before any test

---
+++ KoPrivateGPT/DB/mongo_db.py
@@ -0,0 +1,46 @@
+import pymongo
+from KoPrivateGPT.DB.base import BaseDB
+from KoPrivateGPT.schema import Passage
+
+
+class MongoDB(BaseDB):
+    def __init__(self, mongo_url: str, db_name: str):
+        self.mongo_url = mongo_url
+        self.db_name = db_name
+        self.client = None
+        self.passage_collection = None
+        self.db = None
+
+    @property
+    def db_type(self) -> str:
+        return 'local'
+
+    def create(self, collection_name: str):
+        self.client = pymongo.MongoClient(self.mongo_url)
+        self.db = self.client.get_database(self.db_name)
+        if collection_name in self.db.list_collection_names():
+            raise FileExistsError(f'{collection_name} already exists')
+        if collection_name not in self.db.list_collection_names():
+            self.passage_collection = self.db.create_collection(collection_name)
+
+    def load(self, collection_name: str):
+        if collection_name not in self.db.list_collection_names():
+            raise FileNotFoundError(f'{collection_name} does not exist')
+        self.passage_collection = self.db.get_collection(collection_name)
+
+    def create_or_load(self, collection_name: str):
+        if collection_name in self.db.list_collection_names():
+            self.load(collection_name)
+        else:
+            self.create(collection_name)
+
+    def save(self, passages: list):
+        for passage in passages:
+            self.passage_collection.insert_one(passage.to_document().dict())
+
+    def fetch(self, ids: list) -> list:
+        result = list(self.passage_collection.find({"id": {"$in": ids}}))
+        return result
+
+    def search(self, filter: dict) -> list:
+        raise NotImplementedError("MongoDB does not support search method")
