8f088152b8a5a7d315cdc4e9db8be6fc1e83843d
jeffrey <vkefhdl1@gmail.com>
2023-08-22 23:04:47 +0900

    add query decompose util at utils package

---
+++ KoPrivateGPT/llm/decompose.py
@@ -0,0 +1,17 @@
+"""
+This code is inspired by Visconde paper and its github repo.
+@inproceedings{10.1007/978-3-031-28238-6_44,
+author = {Pereira, Jayr and Fidalgo, Robson and Lotufo, Roberto and Nogueira, Rodrigo},
+title = {Visconde: Multi-Document QA With&nbsp;GPT-3 And&nbsp;Neural Reranking},
+year = {2023},
+isbn = {978-3-031-28237-9},
+publisher = {Springer-Verlag},
+address = {Berlin, Heidelberg},
+url = {https://doi.org/10.1007/978-3-031-28238-6_44},
+doi = {10.1007/978-3-031-28238-6_44},
+booktitle = {Advances in Information Retrieval: 45th European Conference on Information Retrieval, ECIR 2023, Dublin, Ireland, April 2–6, 2023, Proceedings, Part II},
+pages = {534–543},
+numpages = {10},
+location = {Dublin, Ireland}
+}
+"""
