b9dea3a281f6dcd0aecc718b2c69d67f333cb5d6
jeffrey <vkefhdl1@gmail.com>
2023-08-28 22:41:58 +0900

    delete langchain hydeEmbedding at run_localGPT.py

--- run_localGPT.py
+++ run_localGPT.py
@@ -1,9 +1,6 @@
 from typing import List
 
 import click
-from langchain import LLMChain
-from langchain.chains import HypotheticalDocumentEmbedder
-from langchain.prompts import PromptTemplate
 
 from KoPrivateGPT.options import Options, PickleDBOptions
 from KoPrivateGPT.options.config import MongoDBOptions
@@ -29,21 +26,12 @@ def print_docs(docs: List[Passage]):
     print("----------------------------------참조한 문서---------------------------")
 
 
-def hyde_embeddings(llm, base_embedding):
-    hyde_prompt = """
-    다음 질문에 대하여, 적절한 정보가 주어졌다고 가정하고 대답을 생성하세요.
-    질문: {question}
-    답변: """
-    prompt = PromptTemplate(template=hyde_prompt, input_variables=["question"])
-    llm_chain = LLMChain(llm=llm, prompt=prompt)
-    return HypotheticalDocumentEmbedder(llm_chain=llm_chain, base_embeddings=base_embedding)
-
-
 @click.command()
 @click.option('--device_type', default='mps', help='device to run on, select gpu, cpu or mps')
 @click.option('--retrieval_type', default='bm25', help='retrieval type to use, select vectordb or bm25')
 @click.option('--vectordb_type', default='chroma', help='vector database to use, select chroma or pinecone')
-@click.option('--embedding_type', default='ko_sroberta_multitask', help='embedding model to use, select OpenAI or KoSimCSE.')
+@click.option('--embedding_type', default='ko_sroberta_multitask',
+              help='embedding model to use, select OpenAI or KoSimCSE.')
 @click.option('--model_name', default='gpt-3.5-turbo', help='model name to use.')
 @click.option('--api_base', default=None, help='api base to use.')
 @click.option('--db_type', default='mongo_db', help='db type to use, select pickle_db or mongo_db')
