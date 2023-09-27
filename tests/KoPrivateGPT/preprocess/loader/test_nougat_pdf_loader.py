5a375a333482dd9a8b8f9f171ef16c43ce23b094
jeffrey <vkefhdl1@gmail.com>
2023-09-27 14:17:03 +0900

    complete nougat_pdf_loader

--- tests/KoPrivateGPT/preprocess/loader/test_nougat_pdf_loader.py
+++ tests/KoPrivateGPT/preprocess/loader/test_nougat_pdf_loader.py
@@ -13,10 +13,17 @@ file_dir = os.path.join(root_dir, "resources", "ingest_files")
 def nougat_pdf_loader():
     pdf_filepath = os.path.join(file_dir, 'test1.pdf')
     assert os.path.exists(pdf_filepath)
-    assert os.getenv('NOUGAT_HOST')
+    assert bool(os.getenv('NOUGAT_HOST'))
     nougat_pdf_loader = NougatPDFLoader(file_path=pdf_filepath, nougat_host=os.getenv('NOUGAT_HOST'))
     yield nougat_pdf_loader
 
 
 def test_nougat_pdf_loader(nougat_pdf_loader):
-    doc = nougat_pdf_loader.load()
+    docs = nougat_pdf_loader.load()
+    assert len(docs) == 17
+    for doc in docs:
+        assert bool(doc.page_content)
+        if doc.metadata["content_type"] == "table":
+            assert doc.page_content.startswith("\\\\begin{table}")
+        else:
+            assert bool(doc.page_content)
