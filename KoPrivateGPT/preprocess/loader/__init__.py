c7cb7f1a0fb7edb4cbdce46d647fdd66b7b91ed9
jeffrey <vkefhdl1@gmail.com>
2023-09-27 14:53:54 +0900

    add MathpixMarkdownLoader at __init__

--- KoPrivateGPT/preprocess/loader/__init__.py
+++ KoPrivateGPT/preprocess/loader/__init__.py
@@ -2,4 +2,5 @@ from .dataset_loader import KoStrategyQALoader
 from .excel_loader import ExcelLoader
 from .file_loader import FileLoader
 from .hwp_loader import HwpLoader
+from .mathpix_markdown_loader import MathpixMarkdownLoader
 from .nougat_pdf_loader import NougatPDFLoader
