7092fb0bf794fc2dadbd48b71a0931cc27ce0491
jeffrey <vkefhdl1@gmail.com>
2023-10-01 16:25:38 +0900

    move reranker tests

--- tests/KoPrivateGPT/reranker/test_base_reranker.py
+++ tests/KoPrivateGPT/reranker/test_base_reranker.py
@@ -5,7 +5,7 @@ from uuid import UUID
 
 from KoPrivateGPT.schema import Passage
 
-root_dir = pathlib.PurePath(os.path.dirname(os.path.realpath(__file__))).parent.parent.parent
+root_dir = pathlib.PurePath(os.path.dirname(os.path.realpath(__file__))).parent.parent
 with open(os.path.join(root_dir, "resources", "sample_passages.pkl"), 'rb') as r:
     TEST_PASSAGES = pickle.load(r)
 
