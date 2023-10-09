2d5b180cf3141791197465017cff90f3b5b75923
jeffrey <vkefhdl1@gmail.com>
2023-10-09 15:38:11 +0900

    text splitter annotation for documentation

--- KoPrivateGPT/preprocess/text_splitter/text_splitter.py
+++ KoPrivateGPT/preprocess/text_splitter/text_splitter.py
@@ -1,19 +1,32 @@
-from langchain.schema import Document
-from typing import Optional, List, Any
+from typing import Optional, List
 from uuid import uuid4
+
+from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-from KoPrivateGPT.schema import Passage
 from KoPrivateGPT.preprocess.text_splitter.base import BaseTextSplitter
+from KoPrivateGPT.schema import Passage
 
 
 class RecursiveTextSplitter(BaseTextSplitter):
+    """
+    Split a document into passages by recursively splitting on a list of separators.
+    You can specify a window_size and overlap_size to split the document into overlapping passages.
+    """
     def __init__(self, separators: Optional[List[str]] = None,
                  keep_separator: bool = True,
                  *args, **kwargs):
+        """
+        :param separators: A list of strings to split on. Default is None.
+        :param keep_separator: Whether to keep the separator in the passage. Default is True.
+        :param kwargs: Additional arguments to pass to the langchain RecursiveCharacterTextSplitter.
+        """
         self.splitter = RecursiveCharacterTextSplitter(separators, keep_separator, **kwargs)
 
     def split_document(self, document: Document) -> List[Passage]:
+        """
+        Split a document.
+        """
         split_documents = self.splitter.split_documents([document])
         passages = []
         ids = [uuid4() for _ in range(len(split_documents))]
