4083bf920949495f2feac1ab0cdb72d68bcb3ed6
jeffrey <vkefhdl1@gmail.com>
2023-10-09 17:11:15 +0900

    schema annotation for documentation

--- KoPrivateGPT/schema/passage.py
+++ KoPrivateGPT/schema/passage.py
@@ -8,12 +8,19 @@ from pydantic import Field
 
 class Passage(Serializable):
     """Class for storing a passage and metadatas"""
+
     id: Union[UUID, str] = Field(default_factory=uuid4)
+    """Unique identifier for the passage. You can use string or UUID. By default, create new UUID for new passage."""
     content: str
+    """String text."""
     filepath: str
+    """Filepath of the passage."""
     previous_passage_id: Optional[Union[UUID, str]]
+    """Previous passage's id. If this is the first passage, this value should be None."""
     next_passage_id: Optional[Union[UUID, str]]
+    """Next passage's id. If this is the last passage, this value should be None."""
     metadata_etc: dict = Field(default_factory=dict)
+    """Arbitrary metadata about the passage."""
 
     def to_document(self) -> Document:
         metadata = self.metadata_etc.copy()
