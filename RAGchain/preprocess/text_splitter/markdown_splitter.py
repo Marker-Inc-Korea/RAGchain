2832f1d2abbb697a2ce8cbb9167e34c09771f178
minsing <ironman0722@naver.com>
2023-10-21 18:25:48 +0900

    Remove source data of Metadata_etc right form in for loop.

--- RAGchain/preprocess/text_splitter/markdown_splitter.py
+++ RAGchain/preprocess/text_splitter/markdown_splitter.py
@@ -30,20 +30,22 @@ class MarkDownHeaderSplitter(BaseTextSplitter):
 
         self.markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on, return_each_line)
 
-    def split_document(self, documents: Document):
+    def split_document(self, document: Document):
 
-        split_documents = self.markdown_splitter.split_text(documents.page_content)
+        split_documents = self.markdown_splitter.split_text(document.page_content)
 
 
         passages = []
         ids = [uuid4() for _ in range(len(split_documents))]
 
-        filepath = documents.metadata.pop('source')  # user doc's metadata value.
+        filepath = document.metadata.pop('source')  # user doc's metadata value.
+        doc_metadata_etc = document.metadata  # TEST_DOCUMENT's metadata etc
 
         for i, (split_document, uuid) in enumerate(zip(split_documents, ids)):
             # Modify meta_data's keys and values right form.
-            metadata_etc = dict(split_document.metadata.copy(),
-                                **documents.metadata, )  # metadata_etc = doc's metadata_etc + headers
+            ## metadata_etc = doc's metadata_etc + headers
+            metadata_etc = dict(split_document.metadata.copy(),  # Header information
+                                **doc_metadata_etc, )  # TEST_DOCUMENT's metadata etc
 
             previous_passage_id = ids[i - 1] if i > 0 else None
             next_passage_id = ids[i + 1] if i < len(split_documents) - 1 else None
