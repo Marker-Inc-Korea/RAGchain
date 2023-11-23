5bfa56c7e1db03afd992573d52e09564119c64fe
minsing <ironman0722@naver.com>
2023-11-24 00:06:01 +0900

    Modify mr_tydi.py and test_mr_tydi.py some else cases. And modify metrics using self member variables. Modify process of ingesting passages.

--- RAGchain/benchmark/dataset/mr_tydi.py
+++ RAGchain/benchmark/dataset/mr_tydi.py
@@ -1,7 +1,9 @@
+import itertools
 from copy import deepcopy
 from typing import List, Optional
 
 import pandas as pd
+import sklearn
 from datasets import load_dataset
 
 from RAGchain.DB.base import BaseDB
@@ -10,12 +12,6 @@ from RAGchain.pipeline.base import BasePipeline
 from RAGchain.retrieval.base import BaseRetrieval
 from RAGchain.schema import EvaluateResult, Passage
 
-
-# TODO: 진짜 mrr이 rank aware한것인가?
-# TODO: positive passage는 여러개 있는가? 그리고 rank가 있는가> 근데 mrr이 있으면 rank가 있다고 추정
-# TODO: train은 data leakage 일어날 수 있으니 dev로? -> dev는 303개 밖에 없는 데이터
-# TODO: 문제점이 너무 한번 돌리는데 데이터셋 다운받는게 오래걸림
-# TODO: 어떻게 language를 optional 하게 만들것인가?
 
 class MrTydiEvaluator(BaseDatasetEvaluator):
     """
@@ -34,9 +30,18 @@ class MrTydiEvaluator(BaseDatasetEvaluator):
         Supporting metrics are Recall, Precision, Hole, TopK_Accuracy, EM, F1_score, context_recall, context_precision, MRR.
         You must ingest all data for using context_recall and context_precision metrics.
         :param language: The string data which is Mr.tydi dataset language.
+        You can choose languages like below.
+        arabic, bengali, combined, english, finnish, indonesian, japanese, korean, russian, swahili, telugu, thai
         """
-        support_metrics = ['Recall', 'Precision', 'Hole', 'TopK_Accuracy', 'EM', 'F1_score', 'context_recall',
-                           'context_precision', 'MRR']
+        support_metrics = (self.retrieval_gt_metrics + self.retrieval_no_gt_metrics +
+                           ['MRR'])
+        languages = ['arabic', 'bengali', 'combined', 'english', 'finnish',
+                     'indonesian', 'japanese', 'korean', 'russian', 'swahili', 'telugu', 'thai']
+
+        if language not in languages:
+            raise ValueError(f"You input invalid language ({language})."
+                             f"\nPlease input language among below language."
+                             "\n(arabic, bengali, combined, english, finnish, indonesian, japanese, korean, russian, swahili, telugu, thai)")
 
         if metrics is not None:
             using_metrics = list(set(metrics))
@@ -45,6 +50,7 @@ class MrTydiEvaluator(BaseDatasetEvaluator):
         super().__init__(run_all=False, metrics=using_metrics)
 
         self.run_pipeline = run_pipeline
+        self.eval_size = evaluate_size
 
         # Data load
         self.file_path = 'castorini/mr-tydi'
@@ -53,10 +59,8 @@ class MrTydiEvaluator(BaseDatasetEvaluator):
         self.corpus = pd.DataFrame(
             {'docid': corpus['docid'], 'title': corpus['title'], 'text': corpus['text']}
         )
-        # TODO: ingest와 passage변환이 너무 오래걸려서 일시적으로 slice
 
         # Create qa data with train set for query, retrieval_gt
-        # TODO: language가 잘못 들어갔을때의 경우의 수(여러 language를 받았을때-> 인제스트할때 다른 언어 데이터도 같이 인제스트 될 수 있으므로 그냥 사용자가 language알아서 바꿔가면서 돌리는게 좋을듯)
         self.qa_data = pd.DataFrame(
             {'query_id': dataset['query_id'], 'question': dataset['query'],
              'positive_passages': dataset['positive_passages']}
@@ -69,17 +73,45 @@ class MrTydiEvaluator(BaseDatasetEvaluator):
         Ingest dataset to retrievals and db.
         :param retrievals: The retrievals that you want to ingest.
         :param db: The db that you want to ingest.
+        :param ingest_size: The number of data to ingest. If None, ingest all data.
+        If you want to use context_recall and context_precision metrics, you should ingest all data.
         """
 
+        # If ingest size too bit, It takes a long time.(Because corpus size is 1496126!)
+        # So we shuffle corpus and slice by ingest size for test.
+        # Put retrieval gt corpus in passages because retrieval retrieves ground truth in db.
+
+        make_retrieval_gt_passage = deepcopy(self.qa_data['positive_passages'])
         make_passages = deepcopy(self.corpus)
+
+        if ingest_size is not None:
+            # ingest size must be larger than evaluate size.
+            if ingest_size >= self.eval_size:
+                make_passages = sklearn.utils.shuffle(make_passages)[:ingest_size]
+            else:
+                raise ValueError("ingest size must be same or larger than evaluate size")
+
+        # Remove duplicated passages between corpus and retrieval gt for ingesting passages faster.
+        make_retrieval_gt_passage = make_retrieval_gt_passage.apply(self.__make_corpus_passages)
+        make_retrieval_gt_passage, gt_ids = zip(*make_retrieval_gt_passage)
+        id_for_remove_duplicated_corpus = list(itertools.chain.from_iterable(gt_ids))
+
+        # Marking dupicated values in the corpus using retrieval_gt id.
+        mask = make_passages.isin(id_for_remove_duplicated_corpus)
+        # Remove duplicated passages
+        make_passages = make_passages[~mask.any(axis=1)]
+
+        make_retrieval_gt_passage = list(itertools.chain.from_iterable(make_retrieval_gt_passage))
         passages = make_passages.apply(self.__make_corpus_passages, axis=1).tolist()
-        test = passages
+        for gt_passage in make_retrieval_gt_passage:
+            passages.append(gt_passage)
 
         for retrieval in retrievals:
             retrieval.ingest(passages)
         db.create_or_load()
         db.save(passages)
         # TODO: 다른 language로 ingest할때는 그전건 초기화 시켜야함.
+        # TODO: 공식문서 쓸때 내 velog link 걸기(영어로 번역)
 
     def evaluate(self, **kwargs) -> EvaluateResult:
         retrieval_gt = [[passage_id['docid'] for passage_id in passages] for passages in
@@ -93,10 +125,28 @@ class MrTydiEvaluator(BaseDatasetEvaluator):
         )
 
     def __make_corpus_passages(self, row):
-        passage = Passage(
-            id=row['docid'],
-            content=row['text'],
-            filepath=self.file_path,
-            metadata_etc={'title': row['title']}
-        )
-        return passage
+        # retrieval gt to passage for ingest.
+        if type(row) == list:
+            gt_passages = []
+            # gt_id for removing duplicated value
+            gt_id = []
+            for element in row:
+                gt_passages.append(Passage(
+                    id=element['docid'],
+                    content=element['text'],
+                    filepath=self.file_path,
+                    metadata_etc={'title': element['title']}
+                ))
+
+                gt_id.append(element['docid'])
+            return gt_passages, gt_id
+
+        # Corpus to passages
+        else:
+            passage = Passage(
+                id=row['docid'],
+                content=row['text'],
+                filepath=self.file_path,
+                metadata_etc={'title': row['title']}
+            )
+            return passage
--- tests/RAGchain/benchmark/dataset/test_mr_tydi.py
+++ tests/RAGchain/benchmark/dataset/test_mr_tydi.py
@@ -15,34 +15,27 @@ bm25_path = os.path.join(root_dir, 'resources', 'bm25', 'ko_strategy_qa_evaluato
 pickle_path = os.path.join(root_dir, 'resources', 'pickle', 'ko_strategy_qa_evaluator.pkl')
 logger = logging.getLogger(__name__)
 
-
-# TODO: 여기서 language별 test 하면 될듯
 @pytest.fixture
 def mr_tydi_evaluator():
     bm25_retrieval = BM25Retrieval(save_path=bm25_path)
     db = PickleDB(pickle_path)
     llm = BasicLLM(model_name='gpt-3.5-turbo-16k')
     pipeline = BasicRunPipeline(bm25_retrieval, llm)
-    evaluator = MrTydiEvaluator(pipeline, evaluate_size=5,
-                                metrics=['Recall', 'Precision', 'Hole', 'TopK_Accuracy', 'EM', 'F1_score',
-                                         'context_recall',
-                                         'context_precision', 'MRR'],
-                                language='korean')
+    evaluator = MrTydiEvaluator(pipeline, evaluate_size=5)
 
-    evaluator.ingest([bm25_retrieval], db)
+    evaluator.ingest(retrievals=[bm25_retrieval], db=db, ingest_size=20)
     yield evaluator
     if os.path.exists(bm25_path):
         os.remove(bm25_path)
     if os.path.exists(pickle_path):
         os.remove(pickle_path)
 
-
 def test_mr_tydi_evaluator(mr_tydi_evaluator):
     result = mr_tydi_evaluator.evaluate()
 
     assert len(result.each_results) == 5
-    assert result.each_results.iloc[0]['question'] == 'does human hair stop squirrels'
-    assert result.each_results.iloc[0]['answer']
+    assert result.each_results.iloc[0][
+               'question'] == 'Quantum field theory naturally began with the study of electromagnetic interactions, as the electromagnetic field was the only known classical field as of the 1920s.[8]:1", "title": "Quantum field theory'
     for key, value in result.results.items():
         logger.info(f"{key}: {value}")
     logger.info("The result length is " + f"{len(result.results)}")
