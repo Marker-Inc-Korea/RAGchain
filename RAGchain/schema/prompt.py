9966a12d1e5b71f2a8d930dab36352b4e3306246
jeffrey <vkefhdl1@gmail.com>
2023-11-24 18:48:05 +0900

    add RAGchain Chat Prompt Template for validation question and passages

--- RAGchain/schema/prompt.py
+++ RAGchain/schema/prompt.py
@@ -1,7 +1,17 @@
-from langchain.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate, ChatPromptTemplate
 
 
 class RAGchainPromptTemplate(PromptTemplate):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # validate template
+        if "question" not in self.input_variables:
+            raise ValueError("question must be inside prompt as input variable")
+        if "passages" not in self.input_variables:
+            raise ValueError("passages must be inside prompt as input variable")
+
+
+class RAGchainChatPromptTemplate(ChatPromptTemplate):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         # validate template
--- tests/RAGchain/schema/test_ragchain_prompt_template.py
+++ tests/RAGchain/schema/test_ragchain_prompt_template.py
@@ -1,41 +1,44 @@
 import pytest
 
-from RAGchain.schema import RAGchainPromptTemplate
+from RAGchain.schema import RAGchainPromptTemplate, RAGchainChatPromptTemplate
+
+normal_prompt_template = """
+    Answer the question using below passages.
+    Question: {question}
+
+    Passages: {passages}
+    """
+
+no_passage_prompt_template = "Question: {question}"
+no_question_prompt_template = "Answer the question using below passages.\n Passages: {passages}"
+both_missing_prompt_template = "Answer the question using below passages.\nPassage: {passage}"
 
 
 def test_ragchain_prompt_templates():
-    normal_prompt = RAGchainPromptTemplate.from_template(
-        """
-        Answer the question using below passages.
-        Question: {question}
-
-        Passages: {passages}
-        """
-    )
+    normal_prompt = RAGchainPromptTemplate.from_template(normal_prompt_template)
     assert 'question' in normal_prompt.input_variables
     assert 'passages' in normal_prompt.input_variables
 
     with pytest.raises(ValueError):
-        no_passage_prompt = RAGchainPromptTemplate.from_template(
-            """
-            Question: {question}
-            """
-        )
+        no_passage_prompt = RAGchainPromptTemplate.from_template(no_passage_prompt_template)
 
     with pytest.raises(ValueError):
-        no_question_prompt = RAGchainPromptTemplate.from_template(
-            """
-            Answer the question using below passages.
-
-            Passages: {passages}
-            """
-        )
+        no_question_prompt = RAGchainPromptTemplate.from_template(no_question_prompt_template)
 
     with pytest.raises(ValueError):
-        both_missing_prompt = RAGchainPromptTemplate.from_template(
-            """
-            Answer the question using below passages.
+        both_missing_prompt = RAGchainPromptTemplate.from_template(both_missing_prompt_template)
 
-            Passage: {passage}
-            """
-        )
+
+def test_ragchain_chat_prompt_templates():
+    normal_prompt = RAGchainChatPromptTemplate.from_template(normal_prompt_template)
+    assert 'question' in normal_prompt.input_variables
+    assert 'passages' in normal_prompt.input_variables
+
+    with pytest.raises(ValueError):
+        no_passage_prompt = RAGchainChatPromptTemplate.from_template(no_passage_prompt_template)
+
+    with pytest.raises(ValueError):
+        no_question_prompt = RAGchainChatPromptTemplate.from_template(no_question_prompt_template)
+
+    with pytest.raises(ValueError):
+        both_missing_prompt = RAGchainChatPromptTemplate.from_template(both_missing_prompt_template)
