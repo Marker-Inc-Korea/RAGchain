8efad3213b6de11acf180be4384c49bd4925fe09
kimbwook <bwook00@naver.com>
2023-11-29 01:06:06 +0900

    Add page_type in metadata

--- tests/RAGchain/preprocess/loader/test_deepdoctection_pdf_loader.py
+++ tests/RAGchain/preprocess/loader/test_deepdoctection_pdf_loader.py
@@ -70,5 +70,8 @@ def test_deepdoctection_pdf_loader(deepdoctection_pdf_loader):
     print(len(docs))
     assert len(docs) == 15
     assert docs[-1].metadata['page_number'] == 3
-    table_count = sum('<table>' in doc.page_content for doc in docs)
-    assert table_count == 3
+    table_count_content = sum('<table>' in doc.page_content for doc in docs)
+    table_count_metadata = sum('table' in doc.metadata for doc in docs)
+    assert table_count_content == table_count_metadata
+    text_count = sum('text' not in doc.metadata['page_type'] for doc in docs)
+    assert text_count == 12
