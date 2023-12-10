ae8c77f4ac10da8e44610cc1d2824f49985712d4
jeffrey <vkefhdl1@gmail.com>
2023-12-10 18:58:14 +0900

    get REDE training and inference

--- RAGchain/utils/rede_search_detector.py
+++ RAGchain/utils/rede_search_detector.py
@@ -3,6 +3,13 @@ from typing import List, Optional
 import numpy as np
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.schema.embeddings import Embeddings
+from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import roc_curve
+from sklearn.mixture import GaussianMixture
+
+
+def _normalize_vectors(vectors):
+    return vectors / np.linalg.norm(vectors, axis=1, keepdims=True)
 
 
 class RedeSearchDetector:
@@ -34,11 +41,12 @@ class RedeSearchDetector:
         self.mu = None
         self.omega_matrix = None  # Omega matrix for linear transformation.
         self.gmm = None  # Gaussian Mixture Model for classify knowledge-seeking turn.
+        self.norm = None  # Norm for normalize to unit vector.
 
-    def representation_transform(self,
-                                 knowledge_seeking_sentences: List[str],
-                                 L: Optional[int] = None,
-                                 ):
+    def find_representation_transform(self,
+                                      knowledge_seeking_sentences: List[str],
+                                      L: Optional[int] = None,
+                                      ):
         """
         :param knowledge_seeking_sentences: Knowledge-seeking turn sentences. List[str].
         :param L: Number of dimensions of the transformed representation. If None, use whole dimension.
@@ -56,6 +64,100 @@ class RedeSearchDetector:
 
         # TODO: add L truncation
         # find omega matrix
-        self.omega_matrix = np.dot(U, np.sqrt(np.linalg.inv(np.diag(S))))
+        self.omega_matrix = U @ np.sqrt(np.linalg.inv(np.diag(S)))
 
         print("REDE representation transform done.")
+
+    def representation_formation(self, vectors: np.ndarray) -> np.ndarray:
+        """
+        :param vectors: Vectors after encoding. np.ndarray.
+        :return: Transformed vectors. np.ndarray.
+        """
+        return (vectors - self.mu) @ self.omega_matrix
+
+    def train_density_estimation(self,
+                                 gmm: GaussianMixture,
+                                 non_knowledge_seeking_sentences: List[str]):
+        """
+        :param gmm: Gaussian Mixture Model for classify knowledge-seeking turn. GaussianMixture. n_components must be 1.
+        :param non_knowledge_seeking_sentences: Non-knowledge-seeking turn sentences. List[str].
+        """
+        self.gmm = gmm
+        sentence_vectors = np.array(self.embedding.embed_documents(non_knowledge_seeking_sentences))
+        transformed_vectors = np.array(
+            [self.representation_formation(sentence_vector) for sentence_vector in sentence_vectors])
+        # normalize to unit vector
+        transformed_vectors = _normalize_vectors(transformed_vectors)
+
+        self.gmm.fit(transformed_vectors)
+
+    def find_threshold(self,
+                       valid_knowledge_seeking_sentences: List[str],
+                       valid_non_knowledge_seeking_sentences: List[str]):
+        """
+        Find threshold using Youden's index from validation data predictions.
+        :param valid_knowledge_seeking_sentences: knowledge-seeking turn sentences for validation. List[str].
+        You can put same sentences that you used for find_representation_transform function.
+        :param valid_non_knowledge_seeking_sentences: non-knowledge-seeking turn sentences for validation. List[str].
+        """
+        true_scores = self._get_density_score(valid_knowledge_seeking_sentences)
+        false_scores = self._get_density_score(valid_non_knowledge_seeking_sentences)
+
+        y_true = np.concatenate([np.ones_like(true_scores), np.zeros_like(false_scores)])
+        y_score = true_scores + false_scores
+
+        fpr, tpr, thresholds = roc_curve(y_true, y_score)
+        idx = np.argmax(fpr - tpr)
+        self.threshold = thresholds[idx]
+
+        precision, recall, f1 = self._calculate_metrics(y_true, y_score)
+        print(f"Precision: {precision}")
+        print(f"Recall: {recall}")
+        print(f"F1: {f1}")
+
+        return self.threshold
+
+    def detect(self, sentences: List[str]) -> bool:
+        """
+        :param sentences: Sentences to detect. List[str].
+        :return: True if the sentence is knowledge-seeking turn, else False. bool.
+        """
+        score = self._get_density_score(sentences)[0]
+        return score < self.threshold
+
+    def evaluate(self, test_knowledge_seeking_sentences: List[str],
+                 test_non_knowledge_seeking_sentences: List[str]):
+        """
+        Evaluate rede search detector using test dataset.
+        :param test_knowledge_seeking_sentences: knowledge-seeking turn sentences for test. List[str].
+        :param test_non_knowledge_seeking_sentences: non-knowledge-seeking turn sentences for test. List[str].
+        """
+        true_scores = self._get_density_score(test_knowledge_seeking_sentences)
+        false_scores = self._get_density_score(test_non_knowledge_seeking_sentences)
+
+        y_true = np.concatenate([np.ones_like(true_scores), np.zeros_like(false_scores)])
+        y_score = true_scores + false_scores
+
+        precision, recall, f1 = self._calculate_metrics(y_true, y_score)
+        print(f"Precision: {precision}")
+        print(f"Recall: {recall}")
+        print(f"F1: {f1}")
+
+        return precision, recall, f1
+
+    def _get_density_score(self, sentences: List[str]) -> List[float]:
+        sentence_vectors = np.array(self.embedding.embed_documents(sentences))
+        transformed_vectors = np.array([self.representation_formation(np.array(v)) for v in sentence_vectors])
+        transformed_vectors = _normalize_vectors(transformed_vectors)
+        scores = self._score_vectors(transformed_vectors)
+        return scores
+
+    def _score_vectors(self, vectors):
+        return [self.gmm.score(vector.reshape(1, -1)) for vector in vectors]
+
+    def _calculate_metrics(self, y_true, y_score):
+        predictions = np.where(y_score < self.threshold, 1, 0)
+        precision = precision_score(y_true, predictions)
+        recall = recall_score(y_true, predictions)
+        f1 = f1_score(y_true, predictions)
+        return precision, recall, f1
--- tests/RAGchain/utils/rede_search_detector_testing.py
+++ tests/RAGchain/utils/rede_search_detector_testing.py
@@ -1,8 +1,13 @@
 from datasets import load_dataset
 from dotenv import load_dotenv
+from sklearn.mixture import GaussianMixture
 
 load_dotenv()
 from RAGchain.utils.rede_search_detector import RedeSearchDetector
+
+
+def extract_last_log_text(row):
+    return row.log[-1]['text']
 
 
 def main():
@@ -15,9 +20,19 @@ def main():
     non_knowledge_seeking_turns = test_df[test_df['target'] == False]
 
     rede_detector = RedeSearchDetector(threshold=0.5)
-    rede_detector.representation_transform([
+    rede_detector.find_representation_transform([
         row.log[-1]['text'] for i, row in knowledge_seeking_turns.iterrows()
     ])
+    rede_detector.train_density_estimation(
+        GaussianMixture(n_components=1),
+        [row.log[-1]['text'] for i, row in non_knowledge_seeking_turns.iterrows()])
+    validation_dataset = load_dataset(dataset_repo, split='validation')
+    validation_df = validation_dataset.to_pandas()
+    validation_df = validation_df.sample(100, random_state=42)
+    validation_knowledge_seeking_turns = validation_df[validation_df['target']]
+    validation_non_knowledge_seeking_turns = validation_df[validation_df['target'] == False]
+    rede_detector.find_threshold(validation_knowledge_seeking_turns.apply(extract_last_log_text, axis=1).tolist(),
+                                 validation_non_knowledge_seeking_turns.apply(extract_last_log_text, axis=1).tolist())
 
 
 if __name__ == '__main__':
