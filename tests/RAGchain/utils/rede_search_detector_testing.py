dcee77db94546f0c2dc2314799fc73765e109653
jeffrey <vkefhdl1@gmail.com>
2023-12-11 13:00:10 +0900

    benchmark code update

--- tests/RAGchain/utils/rede_search_detector_testing.py
+++ tests/RAGchain/utils/rede_search_detector_testing.py
@@ -11,26 +11,34 @@ def extract_last_log_text(row):
 
 
 def main():
-    dataset_repo = "NomaDamas/DSTC-11-Track-5"
-    test_dataset = load_dataset(dataset_repo, split='test')
-    test_df = test_dataset.to_pandas()
-    test_df = test_df.sample(20, random_state=42)
+    knowledge_turn_cnt = 2
+    non_knowledge_turn_cnt = 50
 
-    knowledge_seeking_turns = test_df[test_df['target']]
-    non_knowledge_seeking_turns = test_df[test_df['target'] == False]
+    dataset_repo = "NomaDamas/DSTC-11-Track-5"
+    train_dataset = load_dataset(dataset_repo, split='train')
+    train_df = train_dataset.to_pandas()
+    train_df = train_df.sample(200, random_state=42)
+
+    knowledge_seeking_turns = train_df[train_df['target']]
+    non_knowledge_seeking_turns = train_df[train_df['target'] == False]
+
+    # slice for testing
+    knowledge_seeking_turns = knowledge_seeking_turns[:knowledge_turn_cnt]
+    non_knowledge_seeking_turns = non_knowledge_seeking_turns[:non_knowledge_turn_cnt]
 
     rede_detector = RedeSearchDetector(threshold=0.5)
     rede_detector.find_representation_transform([
         row.log[-1]['text'] for i, row in knowledge_seeking_turns.iterrows()
-    ], L=1000)
+    ])
     rede_detector.train_density_estimation(
         GaussianMixture(n_components=1),
         [row.log[-1]['text'] for i, row in non_knowledge_seeking_turns.iterrows()])
     validation_dataset = load_dataset(dataset_repo, split='validation')
     validation_df = validation_dataset.to_pandas()
-    validation_df = validation_df.sample(100, random_state=42)
-    validation_knowledge_seeking_turns = validation_df[validation_df['target']]
-    validation_non_knowledge_seeking_turns = validation_df[validation_df['target'] == False]
+    validation_df = validation_df.sample(200, random_state=42)
+    validation_knowledge_seeking_turns = validation_df[validation_df['target']][:int(knowledge_turn_cnt * 0.2) + 1]
+    validation_non_knowledge_seeking_turns = validation_df[validation_df['target'] == False][
+                                             :int(non_knowledge_turn_cnt * 0.2)]
     rede_detector.find_threshold(validation_knowledge_seeking_turns.apply(extract_last_log_text, axis=1).tolist(),
                                  validation_non_knowledge_seeking_turns.apply(extract_last_log_text, axis=1).tolist())
 
