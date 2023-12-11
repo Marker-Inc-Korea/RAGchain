d06e3d522d8395df74ee401b45a3d3d6ee9d196a
minsing <ironman0722@naver.com>
2023-12-11 19:51:37 +0900

    Complete natural_qa.py and test_natural_qa.py.

--- RAGchain/benchmark/dataset/natural_qa.py
+++ RAGchain/benchmark/dataset/natural_qa.py
@@ -1,10 +1,9 @@
 import itertools
+import uuid
 from copy import deepcopy
 from typing import List, Optional
 
-import pandas as pd
 from datasets import load_dataset
-from pandas import json_normalize
 
 from RAGchain.DB.base import BaseDB
 from RAGchain.benchmark.dataset.base import BaseDatasetEvaluator
@@ -21,47 +20,56 @@ class NATURALQAEvaluator(BaseDatasetEvaluator):
     def __init__(self, run_pipeline: BaseRunPipeline,
                  evaluate_size: Optional[int] = None,
                  metrics: Optional[List[str]] = None,
-                 version: str = 'v1.1'):
+                 ):
         """
         :param run_pipeline: The pipeline that you want to benchmark.
         :param evaluate_size: The number of data to evaluate. If None, evaluate all data.
-        MSMARCO dataset we use is huge. Recommend to set proper size for evaluation.
-        :param metrics: The list of metrics to use. If None, use all metrics that supports MSMARCO dataset.
-        :param version: MSMARCO dataset version. You can choose v1.1 or v2.1. Default is v1.1.
+        natural qa dataset we use is huge. Recommend to set proper size for evaluation.
+        :param metrics: The list of metrics to use. If None, use all metrics that supports natural qa dataset.
         Supporting metrics are 'Recall', 'Precision', 'Hole', 'TopK_Accuracy', 'EM', 'F1_score', 'context_recall',
         'context_precision', 'answer_relevancy', 'faithfulness'.
         Rank aware metrics are 'NDCG', 'AP', 'CG', 'IndDCG', 'DCG', 'IndIDCG', 'IDCG', 'RR'.
         You must ingest all data for using context_recall and context_precision metrics.
 
         Notice:
-        Raw natural qa dataset is too large, so we use natural questions short qa by lucadiliello in huggingface.
+        Raw natural qa dataset is too large, so we use natural questions short qa by lucadiliello generated at mrqa 2021 in huggingface.
         """
 
         self.file_path = "lucadiliello/naturalquestionsshortqa"
-        self.dataset = load_dataset(self.file_path, version)
+        self.dataset = load_dataset(self.file_path)['validation'].to_pandas()
 
-        support_metrics = (self.retrieval_gt_metrics + self.retrieval_no_gt_metrics +
-                           self.answer_no_gt_metrics +
-                           self.answer_passage_metrics)
+        default_metrics = (self.retrieval_gt_metrics + self.retrieval_gt_metrics_rank_aware
+                           + self.answer_gt_metrics
+                           + self.answer_no_gt_metrics + self.answer_passage_metrics)
+        support_metrics = (self.retrieval_gt_metrics
+                           + self.retrieval_gt_ragas_metrics + self.retrieval_no_gt_ragas_metrics
+                           + self.answer_gt_metrics + self.answer_no_gt_metrics + self.answer_passage_metrics)
 
         if metrics is not None:
+            # Check if your metrics are available in evaluation datasets.
+            for metric in metrics:
+                if metric not in support_metrics:
+                    raise ValueError("You input metrics that this dataset evaluator not support.")
             using_metrics = list(set(metrics))
         else:
-            using_metrics = support_metrics
+            using_metrics = default_metrics
+
         super().__init__(run_all=False, metrics=using_metrics)
 
         self.eval_size = evaluate_size
         self.run_pipeline = run_pipeline
 
-        # retrieval_gt and retrieval_gt_order will add when make passages.
-        self.qa_data = self.dataset.to_pandas()
+        # Delete duplicated question - answer - retrieval gt
+        self.dataset = self.dataset.groupby('question', as_index=False).agg(
+            {'context': lambda x: list(x), 'key': lambda x: list(x),
+             'answers': lambda x: list(x.tolist()), 'labels': lambda x: list(x)})
 
-        # Remove none answers and is_selected list elements are all 0 and none answer rows.
-        self.qa_data = self.qa_data.loc[
-            (self.qa_data['answers'].map(lambda x: len(x)) != 0) & (
-                    self.qa_data['passages'].map(lambda x: sum(x['is_selected'])) != 0)
-            ].reset_index(drop=True)
-        self.ingest_data = None
+        # Convert key to uuid to retrieve passages' uuid
+        self.context = deepcopy(self.dataset[['key', 'context', 'labels']])
+
+        if evaluate_size is not None and len(self.dataset) > evaluate_size:
+            self.qa_data = self.dataset[:evaluate_size]
+
 
     def ingest(self, retrievals: List[BaseRetrieval], db: BaseDB, ingest_size: Optional[int] = None):
         """
@@ -72,27 +80,17 @@ class NATURALQAEvaluator(BaseDatasetEvaluator):
         If you want to use context_recall and context_precision metrics, you should ingest all data.
         """
 
+        ingest_data = deepcopy(self.context)
         if ingest_size is not None:
-            self.ingest_data = deepcopy(self.qa_data)
             # ingest size must be larger than evaluate size.
             if ingest_size >= self.eval_size:
-                self.ingest_data = self.ingest_data[:ingest_size]
-                make_passages = pd.concat(
-                    [self.ingest_data['query_id'], json_normalize(self.ingest_data['passages'].tolist())],
-                    axis=1)
-
+                ingest_data = ingest_data[:ingest_size]
             else:
                 raise ValueError("ingest size must be same or larger than evaluate size")
-        else:
-            make_passages = pd.concat(
-                [self.ingest_data['query_id'], json_normalize(self.ingest_data['passages'].tolist())],
-                axis=1)
 
         # Create passages.
-        result = make_passages.apply(self.__make_passages_and_retrieval_gt, axis=1)
-        make_passages['passages'], self.ingest_data['retrieval_gt'], self.ingest_data['retrieval_gt_order'] = zip(
-            *result)
-        passages = list(itertools.chain.from_iterable(make_passages['passages']))
+        result = ingest_data.apply(self.__make_passages, axis=1)
+        passages = list(itertools.chain.from_iterable(result))
 
         for retrieval in retrievals:
             retrieval.ingest(passages)
@@ -100,27 +98,28 @@ class NATURALQAEvaluator(BaseDatasetEvaluator):
         db.save(passages)
 
     def evaluate(self, **kwargs) -> EvaluateResult:
-        if self.eval_size is not None and len(self.ingest_data) > self.eval_size:
-            evaluate_data = self.ingest_data[:self.eval_size]
-        else:
-            # else case is ingested data sliced by ingest size.
-            evaluate_data = self.ingest_data
+        question = self.qa_data['question'].tolist()
+        retrieval_gt = [[uuid.UUID(key) for key in key_lst] for key_lst in self.qa_data['key']]
+        answer_gt = [[answer[0] for answer in answer_lst] for answer_lst in self.qa_data['answers']]
 
         return self._calculate_metrics(
-            questions=evaluate_data['question'].tolist(),
+            questions=question,
             pipeline=self.run_pipeline,
-            retrieval_gt=evaluate_data['retrieval_gt'].tolist(),
-            answer_gt=evaluate_data['answers'].tolist(),
+            retrieval_gt=retrieval_gt,
+            answer_gt=answer_gt,
             **kwargs
         )
 
-    def __make_passages_and_retrieval_gt(self, row):
+    def __make_passages(self, row):
+        passages = []
 
-        return Passage(
-            id=row['key'],
-            content=row['context'],
-            filepath=self.file_path,
-            metadata_etc={
-                'url': row['labels']
-            }
-        )
+        for idx in range(len(row['key'])):
+            passages.append(Passage(
+                id=row['key'][idx],
+                content=row['context'][idx],
+                filepath=self.file_path,
+                metadata_etc={
+                    'url': row['labels'][idx]
+                }
+            ))
+        return passages
