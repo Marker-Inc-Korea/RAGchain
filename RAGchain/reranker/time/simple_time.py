f410cf20902e257ad125acbbdd13023e5f323082
jeffrey <vkefhdl1@gmail.com>
2023-12-22 16:04:59 +0900

    add annotation at simple_time.py

--- RAGchain/reranker/time/simple_time.py
+++ RAGchain/reranker/time/simple_time.py
@@ -7,4 +7,9 @@ from RAGchain.schema import Passage
 class SimpleTimeReranker(BaseReranker):
     """Rerank passages by their content_datetime only. It is simple reranker for time-aware RAG."""
     def rerank(self, query: str, passages: List[Passage]) -> List[Passage]:
+        """
+        Rerank passages by their content_datetime only.
+        :param query: query string. It is not used in this reranker. You can put any string in here, no effect to result.
+        :param passages: list of passages to be reranked.
+        """
         return sorted(passages, key=lambda p: p.content_datetime, reverse=True)
