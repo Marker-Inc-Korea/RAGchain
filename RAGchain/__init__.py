e8ab7232189f01c8802e1a5d5a277ce0f87d0aa3
kimbwook <bwook00@naver.com>
2023-12-23 14:06:22 +0900

    Get 'linker_type' from the .env and creates an object called linker (redis or dynamo) that can be used throughout the library.

--- RAGchain/__init__.py
+++ RAGchain/__init__.py
@@ -1,1 +1,16 @@
 __version__ = '0.2.2'
+
+# Sets the linker, which is required to use RAGchain.
+import os
+from dotenv import load_dotenv
+from RAGchain.utils.linker import RedisDBSingleton, DynamoDBSingleton
+
+load_dotenv()
+
+linker_type = os.getenv("LINKER_TYPE")
+if linker_type == "redisdb":
+    linker = RedisDBSingleton()
+elif linker_type == "dynamodb":
+    linker = DynamoDBSingleton()
+else:
+    raise ValueError("Please set LINKER_TYPE to environment variable")
