f0d0d80b236acaf4a4476ae984e41002543489c4
minsing <ironman0722@naver.com>
2023-12-26 17:11:52 +0900

    Modify typo

--- RAGchain/benchmark/dataset/base.py
+++ RAGchain/benchmark/dataset/base.py
@@ -57,7 +57,7 @@ class BaseBeirEvaluator(BaseDatasetEvaluator):
         :param evaluate_size: The number of data to evaluate. If None, evaluate all data.
         We are using train set for evaluating in this class, so it is huge. Recommend to set proper size for evaluation.
 
-        :param metrics: The list of metrics to use. If None, use all metrics that supports BEIR datasets.
+        :param metrics: The list of metrics to use. If None, use all metrics that supports BEIR dataset.
         Supporting metrics are Recall, Precision, Hole, TopK_Accuracy, EM, F1_score, context_precision, MRR.
         You must ingest all data for using context_recall and context_precision metrics.
         Notice: We except context_recall metric that is ragas metric.
