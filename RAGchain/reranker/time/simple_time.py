b7c56014d764e88aab3d952cb9148280f63f096a
jeffrey <vkefhdl1@gmail.com>
2023-12-27 19:33:34 +0900

    do not inherit BaseReranker at SimpleTimeReranker

--- RAGchain/reranker/time/simple_time.py
+++ RAGchain/reranker/time/simple_time.py
@@ -1,15 +1,14 @@
 from typing import List
 
-from RAGchain.reranker.base import BaseReranker
 from RAGchain.schema import Passage
 
 
-class SimpleTimeReranker(BaseReranker):
+class SimpleTimeReranker:
     """Rerank passages by their content_datetime only. It is simple reranker for time-aware RAG."""
-    def rerank(self, query: str, passages: List[Passage]) -> List[Passage]:
+
+    def rerank(self, passages: List[Passage]) -> List[Passage]:
         """
         Rerank passages by their content_datetime only.
-        :param query: query string. It is not used in this reranker. You can put any string in here, no effect to result.
         :param passages: list of passages to be reranked.
         """
         return sorted(passages, key=lambda p: p.content_datetime, reverse=True)
