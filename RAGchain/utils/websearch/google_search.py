ef3b9c50d0d9546584ce30c6eeb8710dbfd20c4c
kimbwook <bwook00@naver.com>
2023-12-30 21:53:45 +0900

    change code comments

--- RAGchain/utils/websearch/google_search.py
+++ RAGchain/utils/websearch/google_search.py
@@ -11,7 +11,7 @@ from RAGchain.schema import Passage
 class GoogleSearch(BaseWebSearch):
     """
     Langchain's 'GoogleSearchAPIWrapper' returns a List[Dict[str, str]] as the return value.
-    This GoogleSearchWrapper class wraps this return value in a Passage.
+    This GoogleSearch class wraps this return value in a Passage.
     """
     def __init__(self):
         self.search = GoogleSearchAPIWrapper()
