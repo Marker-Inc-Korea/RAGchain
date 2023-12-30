7e25cd459960a48083ff4814a344509198efd78e
jeffrey <vkefhdl1@gmail.com>
2023-12-30 22:04:16 +0900

    change inheritance to Runnable

--- RAGchain/utils/compressor/llm_lingua.py
+++ RAGchain/utils/compressor/llm_lingua.py
@@ -4,26 +4,37 @@ from langchain.prompts.base import StringPromptValue
 from langchain.prompts.chat import ChatPromptValueConcrete, ChatPromptValue
 from langchain.schema.language_model import LanguageModelInput
 from langchain.schema.messages import AnyMessage
-from langchain.schema.runnable import RunnableSerializable, RunnableConfig
+from langchain.schema.runnable import RunnableConfig, Runnable
 from langchain.schema.runnable.utils import Input, Output
-from pydantic import Field
 
 
-class LLMLinguaCompressor(RunnableSerializable[LanguageModelInput, str]):
-    try:
-        from llmlingua import PromptCompressor
-    except ImportError:
-        raise ImportError("Please install llmlingua first.")
+class LLMLinguaCompressor(Runnable[LanguageModelInput, str]):
+    def __init__(self,
+                 model_name: str = "NousResearch/Llama-2-7b-hf",
+                 device_map: str = "cuda",
+                 model_config: dict = {},
+                 open_api_config: dict = {},
+                 **kwargs: Any):
+        """
+        Initialize the LLMLinguaCompressor class.
 
-    model_name: str = "NousResearch/Llama-2-7b-hf"
-    device_map: str = "cuda"
-    model_config: dict = Field(default={})
-    open_api_config: dict = Field(default={})
-    compressor = PromptCompressor(model_name=model_name,
-                                  device_map=device_map,
-                                  model_config=model_config,
-                                  open_api_config=open_api_config)
-    compress_option: dict = Field(default={})
+        :param model_name: The name of the model to be used for compression.
+        :param device_map: The device to be used for compression.
+        :param model_config: The model configuration to be used for compression.
+        :param open_api_config: The open api configuration to be used for compression.
+        :param kwargs: The keyword arguments to be used for compression. parameter of compress_prompt method.
+        """
+        super().__init__()
+        try:
+            from llmlingua import PromptCompressor
+        except ImportError:
+            raise ImportError("Please install llmlingua first.")
+
+        self.compressor = PromptCompressor(model_name=model_name,
+                                           device_map=device_map,
+                                           model_config=model_config,
+                                           open_api_config=open_api_config)
+        self.compress_option = kwargs
 
     def invoke(self, input: Input, config: Optional[RunnableConfig] = None,
                **kwargs: Any) -> Output:
