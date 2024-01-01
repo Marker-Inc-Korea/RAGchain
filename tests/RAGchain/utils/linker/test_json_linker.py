7cc359234d2df21f5437257873c87b18aca1d9bd
kimbwook <bwook00@naver.com>
2024-01-01 14:51:17 +0900

    delete redundant '()'

--- tests/RAGchain/utils/linker/test_json_linker.py
+++ tests/RAGchain/utils/linker/test_json_linker.py
@@ -4,7 +4,7 @@ from RAGchain.utils.linker import JsonLinker
 import test_base_linker
 
 
-@pytest.fixture()
+@pytest.fixture
 def json_linker():
     json_linker = JsonLinker(allow_multiple_instances=True)
     json_linker.put_json(test_base_linker.TEST_IDS[0], test_base_linker.TEST_DB_ORIGIN)
