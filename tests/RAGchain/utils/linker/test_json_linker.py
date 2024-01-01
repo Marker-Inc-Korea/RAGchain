5131c0484be768cbb9ec822388ccb18f2b4b40df
kimbwook <bwook00@naver.com>
2024-01-01 19:38:24 +0900

    [json_linker]Make test code at base and just replace linker instance for testing.

--- tests/RAGchain/utils/linker/test_json_linker.py
+++ tests/RAGchain/utils/linker/test_json_linker.py
@@ -4,6 +4,10 @@ from RAGchain.utils.linker import JsonLinker
 from RAGchain.utils.linker.base import NoIdWarning, NoDataWarning
 import test_base_linker
 
+TEST_UUID_IDS = test_base_linker.TEST_UUID_IDS
+TEST_UUID_STR_IDS = test_base_linker.TEST_UUID_STR_IDS
+TEST_STR_IDS = test_base_linker.TEST_STR_IDS
+
 
 @pytest.fixture
 def json_linker():
@@ -12,24 +16,8 @@ def json_linker():
     json_linker.flush_db()
 
 
-def test_get_json_UUID_UUID(json_linker):
-    json_linker.put_json(test_base_linker.TEST_UUID_IDS[0], test_base_linker.TEST_DB_ORIGIN)
-    assert json_linker.get_json(test_base_linker.TEST_UUID_IDS) == [test_base_linker.TEST_DB_ORIGIN]
-
-
-def test_get_json_UUID_STR(json_linker):
-    json_linker.put_json(test_base_linker.TEST_UUID_IDS[0], test_base_linker.TEST_DB_ORIGIN)
-    assert json_linker.get_json(test_base_linker.TEST_STR_IDS) == [test_base_linker.TEST_DB_ORIGIN]
-
-
-def test_get_json_STR_UUID(json_linker):
-    json_linker.put_json(test_base_linker.TEST_STR_IDS[0], test_base_linker.TEST_DB_ORIGIN)
-    assert json_linker.get_json(test_base_linker.TEST_UUID_IDS) == [test_base_linker.TEST_DB_ORIGIN]
-
-
-def test_get_json_STR_STR(json_linker):
-    json_linker.put_json(test_base_linker.TEST_STR_IDS[0], test_base_linker.TEST_DB_ORIGIN)
-    assert json_linker.get_json(test_base_linker.TEST_STR_IDS) == [test_base_linker.TEST_DB_ORIGIN]
+def test_get_json(json_linker):
+    test_base_linker.get_json_test(json_linker, TEST_UUID_IDS, TEST_UUID_STR_IDS, TEST_STR_IDS)
 
 
 def test_no_id_warning(json_linker):
