b388d5c8dff82730223d3b127a9435fa33ac2681
Bwook (Byoungwook) Kim <bwook00@naver.com>
2024-01-02 18:24:35 +0900

    [Hotfix] Json Linker can get db_path properly when use UUID id (#430)
    * [Hotfix]Fix to automatically convert uuid to string to get db_path by uuid
    
    * [Hotfix]Fix to automatically convert uuid to string to get db_path by uuid
    
    * delete json_linker.flush_db() because conftest.py does linker flush at end of pytest
    
    * reuse json_linker.flush_db()
    
    * delete redundant '()'
    
    * Create NoIdWarning to raise Warning when there are no id in linker
    
    Create NoDataWarning to raise Warning when there are no data in linker for given id.
    
    * Adjust NoIdWarning, NoDataWarning in redis_linker.py
    
    * Adjust NoIdWarning, NoDataWarning in dynamo_linker.py
    
    * Make test code to response more cases.
    - Input : uuid => search as uuid
    - Input: uuid => search as str
    - Input: str => search as str
    - Input: str => search as uuid
    
    * append 'None' when db_origin is None
    
    * Create delete_json function in json_linker.py
    
    * Add __init__.py NoIdWarning, NoDataWarning
    
    * Add three types test ids
    
    * fix redis_linker.py data == 'null'
    
    * [json_linker]Make test code at base and just replace linker instance for testing.
    
    * create delete_json function in redis, dynamo too
    
    * Edit Warning message. don't use specific linker name
    
    * resolve duplicate test problem
    
    * Use data.keys() in json_linker's id check
    
    * Using mget(), solved the problem of having to blow the query every time when using get() in redis_linker.py
    
    * Using mget(), solved the problem of having to blow the query every time when using get() in redis_linker.py
    (but mset() is only available in redisDB with additional support for JSON modules).
    
    Edit test code
    
    * [json linker]Make it possible to PUT and GET at once with an ID list. Test code pattern create
    
    * [dynamo linker]Make it possible to PUT and GET at once with an ID list. Test code pattern create
    
    * Linker put_json can be put at once, so I changed it where I save it in the DB.
    
    * change type annotation to typing.List for version compatibility
    
    * refactor dynamo_linker put_json()
    
    * fix get_json that batch_get_item shuffle the order of responses
    
    * refactor json_linker get_json() method
    
    * refactor redis_linker a bit
    
    * refactor redis get_json and change delete_json to List
    
    * make long test
    
    * make dynamo_linker delete_json()
    
    ---------
    
    Co-authored-by: jeffrey <vkefhdl1@gmail.com>

--- RAGchain/utils/linker/__init__.py
+++ RAGchain/utils/linker/__init__.py
@@ -2,3 +2,5 @@ from .redis_linker import RedisLinker
 from .dynamo_linker import DynamoLinker
 from .json_linker import JsonLinker
 from .base import SingletonCreationError
+from .base import NoIdWarning
+from .base import NoDataWarning
--- tests/RAGchain/utils/linker/test_dynamo_linker.py
+++ tests/RAGchain/utils/linker/test_dynamo_linker.py
@@ -1,16 +1,42 @@
+import time
+
 import pytest
 
-from RAGchain.utils.linker import DynamoLinker
 import test_base_linker
+from RAGchain.utils.linker import DynamoLinker
+
+TEST_UUID_IDS = test_base_linker.TEST_UUID_IDS
+TEST_UUID_STR_IDS = test_base_linker.TEST_UUID_STR_IDS
+TEST_STR_IDS = test_base_linker.TEST_STR_IDS
 
 
 @pytest.fixture
 def dynamo_db():
     dynamo_db = DynamoLinker(allow_multiple_instances=True)
-    dynamo_db.put_json(test_base_linker.TEST_IDS[0], test_base_linker.TEST_DB_ORIGIN)
     yield dynamo_db
     dynamo_db.flush_db()
+    time.sleep(5)
 
 
 def test_get_json(dynamo_db):
-    assert dynamo_db.get_json(test_base_linker.TEST_IDS) == [test_base_linker.TEST_DB_ORIGIN]
+    test_base_linker.get_json_test(dynamo_db, TEST_UUID_IDS, TEST_UUID_STR_IDS, TEST_STR_IDS)
+
+
+def test_no_id_warning(dynamo_db):
+    test_base_linker.no_id_warning_test(dynamo_db)
+
+
+def test_no_data_warning(dynamo_db):
+    test_base_linker.no_data_warning_test(dynamo_db)
+
+
+def test_no_data_warning2(dynamo_db):
+    test_base_linker.no_data_warning_test2(dynamo_db)
+
+
+def test_delete(dynamo_db):
+    test_base_linker.delete_test(dynamo_db)
+
+
+def test_long(dynamo_db):
+    test_base_linker.long_test(dynamo_db)
