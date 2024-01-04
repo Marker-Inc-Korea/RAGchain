44b35d2a0d7c4cefcf3df5207a204871e6ec2207
jeffrey <vkefhdl1@gmail.com>
2024-01-04 22:23:54 +0900

    make MonoT5Reranker to runnable

--- tests/RAGchain/reranker/test_mono_t5_reranker.py
+++ tests/RAGchain/reranker/test_mono_t5_reranker.py
@@ -1,7 +1,12 @@
 import pytest
+from langchain_core.runnables import RunnableLambda
 
 import test_base_reranker
 from RAGchain.reranker import MonoT5Reranker
+from RAGchain.schema import RetrievalResult
+
+test_passages = test_base_reranker.TEST_PASSAGES[:20]
+query = "What is query decomposition?"
 
 
 @pytest.fixture
@@ -11,8 +16,20 @@ def mono_t5_reranker():
 
 
 def test_mono_t5_reranker(mono_t5_reranker):
-    test_passages = test_base_reranker.TEST_PASSAGES[:20]
-    query = "What is query decomposition?"
     rerank_passages = mono_t5_reranker.rerank(query, test_passages)
     assert len(rerank_passages) == len(test_passages)
     assert rerank_passages[0] != test_passages[0] or rerank_passages[-1] != test_passages[-1]
+
+
+def test_mono_t5_reranker_runnable(mono_t5_reranker):
+    runnable = mono_t5_reranker | RunnableLambda(lambda x: x.to_dict())
+    result = runnable.invoke(RetrievalResult(
+        query=query,
+        passages=test_passages,
+        scores=[],
+    ))
+    assert len(result['passages']) == len(test_passages)
+    assert result['passages'][0] != test_passages[0] or result['passages'][-1] != test_passages[-1]
+    assert len(result['passages']) == len(result['scores'])
+    for i in range(1, len(result['scores'])):
+        assert result['scores'][i - 1] >= result['scores'][i]
