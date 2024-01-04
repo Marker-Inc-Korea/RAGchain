2b85caef016f0b1c25814e159b43f6de82f78db4
jeffrey <vkefhdl1@gmail.com>
2024-01-04 18:46:41 +0900

    make SimpleTimeReranker to runnable

--- RAGchain/reranker/time/simple_time.py
+++ RAGchain/reranker/time/simple_time.py
@@ -1,10 +1,20 @@
-from typing import List
+from typing import List, Optional
 
+from langchain_core.runnables import RunnableConfig
+from langchain_core.runnables.utils import Input, Output
+
+from RAGchain.reranker.base import BaseReranker
 from RAGchain.schema import Passage
 
 
-class SimpleTimeReranker:
+class SimpleTimeReranker(BaseReranker):
     """Rerank passages by their content_datetime only. It is simple reranker for time-aware RAG."""
+
+    def invoke(self, input: Input, config: Optional[RunnableConfig] = None) -> Output:
+        passages = input.passages
+        sorted_passages = sorted(passages, key=lambda p: p.content_datetime, reverse=True)
+        input.passages = sorted_passages
+        return input
 
     def rerank(self, passages: List[Passage]) -> List[Passage]:
         """
--- tests/RAGchain/reranker/test_simple_time_reranker.py
+++ tests/RAGchain/reranker/test_simple_time_reranker.py
@@ -1,7 +1,9 @@
 import pytest
+from langchain_core.runnables import RunnableLambda
 
 import test_base_reranker
 from RAGchain.reranker.time import SimpleTimeReranker
+from RAGchain.schema import RetrievalResult
 
 
 @pytest.fixture
@@ -12,6 +14,18 @@ def simple_time_reranker():
 
 def test_simple_time_reranker(simple_time_reranker):
     rerank_passages = simple_time_reranker.rerank(test_base_reranker.TEST_PASSAGES)
+    assert len(rerank_passages) == len(test_base_reranker.TEST_PASSAGES)
+    for i in range(1, len(rerank_passages)):
+        assert rerank_passages[i].content_datetime <= rerank_passages[i - 1].content_datetime
+
+
+def test_simple_time_reranker_runnable(simple_time_reranker):
+    runnable = simple_time_reranker | RunnableLambda(lambda x: x.passages)
+    rerank_passages = runnable.invoke(RetrievalResult(
+        query="What is reranker role?",
+        passages=test_base_reranker.TEST_PASSAGES,
+        scores=[1.0] * len(test_base_reranker.TEST_PASSAGES)
+    ))
     assert len(rerank_passages) == len(test_base_reranker.TEST_PASSAGES)
     for i in range(1, len(rerank_passages)):
         assert rerank_passages[i].content_datetime <= rerank_passages[i - 1].content_datetime
