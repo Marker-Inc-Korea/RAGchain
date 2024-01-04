76e6809eb85ca6df6ed5c06b8d03327942133e4a
jeffrey <vkefhdl1@gmail.com>
2024-01-04 23:58:57 +0900

    refactor RerankRunPipeline to runnable

--- RAGchain/pipeline/rerank.py
+++ RAGchain/pipeline/rerank.py
@@ -1,4 +1,3 @@
-from operator import itemgetter
 from typing import List, Optional, Union
 
 from langchain.schema import StrOutputParser
@@ -8,7 +7,7 @@ from langchain.schema.runnable import RunnableLambda
 from RAGchain.pipeline.base import BaseRunPipeline
 from RAGchain.reranker.base import BaseReranker
 from RAGchain.retrieval.base import BaseRetrieval
-from RAGchain.schema import Passage, RAGchainChatPromptTemplate, RAGchainPromptTemplate
+from RAGchain.schema import Passage, RAGchainChatPromptTemplate, RAGchainPromptTemplate, RetrievalResult
 
 
 class RerankRunPipeline(BaseRunPipeline):
@@ -35,7 +34,6 @@ class RerankRunPipeline(BaseRunPipeline):
 
     def __init__(self, retrieval: BaseRetrieval, reranker: BaseReranker, llm: BaseLanguageModel,
                  prompt: Optional[Union[RAGchainPromptTemplate, RAGchainChatPromptTemplate]] = None,
-                 retrieval_option: Optional[dict] = None,
                  use_passage_count: int = 5):
         """
         Initializes an instance of the RerankRunPipeline class.
@@ -43,35 +41,36 @@ class RerankRunPipeline(BaseRunPipeline):
         :param retrieval: An instance of the Retrieval module used for retrieving passages.
         :param reranker: An instance of the Reranker module used for reranking passages.
         :param llm: An instance of the Langchain LLM module used for generating answers.
-        :param retrieval_option: An optional dictionary of options for the retrieval module. Default is top_k=100.
         :param use_passage_count: The number of passages to use for llm question after reranking. Default is 5.
         """
         self.retrieval = retrieval
         self.reranker = reranker
         self.llm = llm
         self.prompt = self._get_default_prompt(llm, prompt)
-        self.retrieval_option = retrieval_option if retrieval_option is not None else {"top_k": 100}
         self.use_passage_count = use_passage_count
         super().__init__()
 
     def _make_runnable(self):
-        self.run = {
-                       "passages": itemgetter("question") | RunnableLambda(lambda question: Passage.make_prompts(
-                           self.__get_passages(question)[0])),
-                       "question": itemgetter("question"),
-                   } | self.prompt | self.llm | StrOutputParser()
+        def __preprocess(result: RetrievalResult) -> dict:
+            result.passages = result.passages[:self.use_passage_count]
+            return result.to_prompt_input()
 
-    def get_passages_and_run(self, questions: List[str]) -> tuple[List[str], List[List[Passage]], List[List[float]]]:
-        passages, rel_scores = map(list, zip(*[self.__get_passages(question) for question in questions]))
-        runnable = {
-                       "question": itemgetter("question"),
-                       "passages": itemgetter("passages") | RunnableLambda(lambda x: Passage.make_prompts(x))
-                   } | self.prompt | self.llm | StrOutputParser()
-        answers = runnable.batch([{"question": question, "passages": passage_group} for question, passage_group in
-                                  zip(questions, passages)])
+        self.run = self.retrieval | self.reranker | RunnableLambda(
+            __preprocess) | self.prompt | self.llm | StrOutputParser()
+
+    def get_passages_and_run(self, questions: List[str], top_k: int = 5) -> tuple[
+        List[str], List[List[Passage]], List[List[float]]]:
+        def __cut_passages(result: RetrievalResult) -> RetrievalResult:
+            result.passages = result.passages[:self.use_passage_count]
+            result.scores = result.scores[:self.use_passage_count]
+            return result
+
+        runnable = self.retrieval | self.reranker | RunnableLambda(__cut_passages) | {
+            "passages": RunnableLambda(lambda x: x.passages),
+            "scores": RunnableLambda(lambda x: x.scores),
+            "answers": RunnableLambda(RetrievalResult.to_prompt_input) | self.prompt | self.llm | StrOutputParser()
+        }
+        result = runnable.batch([(question, top_k) for question in questions])
+        answers, passages, rel_scores = zip(
+            *[(answer['answers'], answer['passages'], answer['scores']) for answer in result])
         return answers, passages, rel_scores
-
-    def __get_passages(self, question: str):
-        passages = self.retrieval.retrieve(question, **self.retrieval_option)
-        reranked_passages = self.reranker.rerank(question, passages)[:self.use_passage_count]
-        return reranked_passages, [i / len(reranked_passages) for i in range(len(reranked_passages), 0, -1)]
