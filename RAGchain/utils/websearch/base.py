909dc4cbaf60eb51b2bcd8497643aa9594ac85f5
jeffrey <vkefhdl1@gmail.com>
2024-01-05 15:34:01 +0900

    refactor BaseWebSearch to runnable

--- RAGchain/utils/websearch/base.py
+++ RAGchain/utils/websearch/base.py
@@ -1,16 +1,43 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Optional, List
+from typing import Optional, List, Tuple, Union, Type
 
-from RAGchain.schema import Passage
+from langchain_core.runnables import Runnable, RunnableConfig
+from langchain_core.runnables.utils import Input, Output
+
+from RAGchain.schema import Passage, RetrievalResult
 
 
-class BaseWebSearch(ABC):
+class BaseWebSearch(Runnable[Union[str, Tuple[str, int]], RetrievalResult], ABC):
     """
     Abstract class for using a web search engine for passage contents.
     """
+
     @abstractmethod
-    def get_search_data(self, query: str, num_results: int = 5,) -> List[Passage]:
+    def get_search_data(self, query: str, num_results: int = 5, ) -> List[Passage]:
         """
         Abstract method for searching passages from the web search engine.
         """
         pass
+
+    def invoke(self, input: Input, config: Optional[RunnableConfig] = None) -> Output:
+        if isinstance(input, str):
+            passages = self.get_search_data(input)
+            return RetrievalResult(query=input, passages=passages, scores=self.__make_scores(len(passages)))
+        elif isinstance(input, Tuple):
+            passages = self.get_search_data(input[0], num_results=input[1])
+            return RetrievalResult(query=input[0], passages=passages, scores=self.__make_scores(len(passages)))
+        else:
+            raise ValueError(f"Input type must be str or Tuple[str, int], but got {type(input)}")
+
+    @property
+    def InputType(self) -> Type[Input]:
+        return Union[str, Tuple[str, int]]
+
+    @property
+    def OutputType(self) -> Type[Output]:
+        return List[Passage]
+
+    @staticmethod
+    def __make_scores(retrieved_length: int):
+        # Make scores with range of 1 to 0. The length must be retrieve_length.
+        return [i / retrieved_length for i in range(retrieved_length, 0, -1)]
--- tests/RAGchain/utils/websearch/test_google_search.py
+++ tests/RAGchain/utils/websearch/test_google_search.py
@@ -1,4 +1,4 @@
-import pytest
+from langchain_core.runnables import RunnableLambda
 
 from RAGchain.utils.websearch import GoogleSearch
 
@@ -7,3 +7,15 @@ def test_google_search():
     search = GoogleSearch()
     passages = search.get_search_data("뉴진스 민지의 생일은?", num_results=2)
     assert len(passages) == 2
+
+
+def test_google_search_runnable():
+    search = GoogleSearch()
+    runnable = search | RunnableLambda(lambda x: x.to_dict())
+    result = runnable.invoke(("뉴진스 민지의 생일은?", 2))
+    assert isinstance(result['query'], str)
+    assert result['query'] == "뉴진스 민지의 생일은?"
+    assert isinstance(result['passages'], list)
+    assert len(result['passages']) == 2
+    assert isinstance(result['scores'], list)
+    assert result['scores'] == [1.0, 0.5]
